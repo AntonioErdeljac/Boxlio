9e711148e22a26be17da2c4cb677187a5f2a5cab
Antonio <hedhogs@gmail.com>
2017-11-29 19:48:59 +0100

    Added image to Opinion component

--- frontend/src/components/Main/Profile/Opinion.js
+++ frontend/src/components/Main/Profile/Opinion.js
@@ -4,6 +4,7 @@ const Opinion = props => {
 	return (
 		<div className="card col-12 my-3">
 			<div className="card-body">
+				<img src={props.opinion.author.image} height="50" style={{borderRadius: '50%'}} />
 				<div className="card-text">{props.opinion.text}</div>
 			</div>
 		</div>
