a3a396e488e5219f910a9612e7e06080e15b26c9
Antonio <hedhogs@gmail.com>
2017-12-05 11:14:25 +0100

    added react-native-maps to Main component

--- BoxlioAndroid/android/app/src/main/java/com/boxlioandroid/MainApplication.java
+++ BoxlioAndroid/android/app/src/main/java/com/boxlioandroid/MainApplication.java
@@ -3,10 +3,13 @@ package com.boxlioandroid;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.airbnb.android.react.maps.MapsPackage;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new MapsPackage()
       );
     }
 
--- BoxlioAndroid/src/components/Main/index.js
+++ BoxlioAndroid/src/components/Main/index.js
@@ -2,33 +2,52 @@ import React from "react";
 import {Container} from "native-base";
 import {Text} from "react-native";
 import { Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import MapView from 'react-native-maps';
+import {StyleSheet} from "react-native";
 
 
 class Main extends React.Component{
 	render(){
 		return (
-			<Container>
-			<Content />
+			<Container style={styles.container}>
+				<MapView style={styles.map} />
+			<Content >
+			</Content>
 				<Footer>
-          <FooterTab style={{backgroundColor: '#1fcf7c'}}>
-            <Button style={{backgroundColor: '#1fcf7c'}}>
-              <Icon style={{color: '#fff'}} name="globe" />
-            </Button>
-            <Button style={{backgroundColor: '#1fcf7c'}}>
-              <Icon style={{color: '#fff'}} name="ios-compass-outline" />
-            </Button>
-            <Button style={{backgroundColor: '#1fcf7c'}}>
-              <Icon style={{color: '#fff'}} name="person" />
-            </Button>
-          </FooterTab>
-        </Footer>
+		          <FooterTab style={{backgroundColor: '#1fcf7c'}}>
+		            <Button style={{backgroundColor: '#1fcf7c'}}>
+		              <Icon style={{color: '#fff'}} name="globe" />
+		            </Button>
+		            <Button style={{backgroundColor: '#1fcf7c'}}>
+		              <Icon style={{color: '#fff', opacity: 0.7}} name="ios-compass-outline" />
+		            </Button>
+		            <Button style={{backgroundColor: '#1fcf7c'}}>
+		              <Icon style={{color: '#fff', opacity: 0.7}} name="person" />
+		            </Button>
+		          </FooterTab>
+		        </Footer>
 			</Container>
 		);
 	}
 }
 
-const style = {
-
-}
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default Main;
