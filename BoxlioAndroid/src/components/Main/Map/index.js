3d71ada6ca55f02859d92dad31f573ccf79c10f1
Antonio <hedhogs@gmail.com>
2017-12-06 16:42:36 +0100

    Added view that will be used to search places in Map component

--- BoxlioAndroid/src/components/Main/Map/index.js
+++ BoxlioAndroid/src/components/Main/Map/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {Container} from "native-base";
-import {Text} from "react-native";
-import { Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
-import MapView from 'react-native-maps';
-import {StyleSheet} from "react-native";
+import { Header, Content, Footer, FooterTab, Button, Icon, Card, CardItem, Body, Text } from 'native-base';
+import {Grid, Col, Row} from "react-native-easy-grid";
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, Dimensions, View} from "react-native";
 import * as Animatable from "react-native-animatable";
+import mapStyles from "./mapStyles";
 const ContainerAnimatable = Animatable.createAnimatableComponent(Container);
 
 class Map extends React.Component{
@@ -12,7 +13,20 @@ class Map extends React.Component{
 		return (
 			<ContainerAnimatable animation="fadeInUp" style={styles.container}>
         <Container style={styles.container}>
-				<MapView style={styles.map} />
+          <View style={styles.searchTo}>
+            <Grid style={{justifyContent: 'space-around', alignItems: 'center'}}>
+              <Col size={1}>
+                <Icon name="ios-home" />
+              </Col>
+              <Col>
+                <Text>test</Text>
+              </Col>
+            </Grid>
+          </View>
+				<MapView 
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={mapStyles}
+        style={styles.map} />
         </Container>
 			</ContainerAnimatable>
 		);
@@ -20,6 +34,20 @@ class Map extends React.Component{
 }
 
 const styles = StyleSheet.create({
+  searchTo: {
+    zIndex: 1000,
+    height: 45 ,
+    width: Dimensions.get('window').width-30,
+    padding: 25,
+    borderRadius: 3,
+    elevation: 3,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    shadowColor: '#000',
+    position: 'absolute',
+    top: 10,
+    backgroundColor: '#fff'
+  },
   container: {
     position: 'absolute',
     top: 0,
