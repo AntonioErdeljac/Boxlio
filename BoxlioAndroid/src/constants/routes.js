77ccb7381e8782b222349ae6ee528b585da39ba7
Antonio Erdeljac <hedhogs@gmail.com>
2018-02-08 23:08:58 +0100

    route change

--- BoxlioAndroid/src/constants/routes.js
+++ BoxlioAndroid/src/constants/routes.js
@@ -1,4 +1,4 @@
 export const
-    API_ROOT = 'https://6fc74237.ngrok.io',
+    API_ROOT = 'https://34ccf5ee.ngrok.io',
     TESCO_API = 'https://dev.tescolabs.com/grocery/products/',
     TESCO_TOKEN = '2888ade9e3d24fdfb07f564ef1317f49'
